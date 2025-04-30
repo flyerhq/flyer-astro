7d2f085a1eb8f2e03128bdbb9716a4aacca7a419
Kacper Grzeszczyk <kacgrzes@gmail.com>
2025-04-30 10:34:14 +0200

    Move all the docs to docs/flutter

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -3,28 +3,40 @@ import type { StarlightUserConfig } from "@astrojs/starlight/types";
 export const sidebar = [
   {
     label: "Introduction",
-    slug: "docs/introduction",
+    slug: "docs/flutter/introduction",
   },
   {
     label: "Getting Started",
     items: [
       // Each item here is one entry in the navigation menu.
-      { label: "Simple Example", slug: "docs/getting-started/simple-example" },
+      {
+        label: "Simple Example",
+        slug: "docs/flutter/getting-started/simple-example",
+      },
       {
         label: "Flyer Chat Architecture",
-        slug: "docs/getting-started/architecture",
+        slug: "docs/flutter/getting-started/architecture",
       },
-      { label: "Customisation", slug: "docs/getting-started/customisation" },
-      { label: "Migration from V1", slug: "docs/getting-started/migration" },
+      {
+        label: "Customisation",
+        slug: "docs/flutter/getting-started/customisation",
+      },
+      {
+        label: "Migration from V1",
+        slug: "docs/flutter/getting-started/migration",
+      },
     ],
   },
   {
     label: "Guides",
     items: [
       // Each item here is one entry in the navigation menu.
-      { label: "Status Indicators", slug: "docs/guides/status-indicators" },
-      { label: "Dynamic Theming", slug: "docs/guides/dynamic-theming" },
-      { label: "More Guides", slug: "docs/guides/more-guides" },
+      {
+        label: "Status Indicators",
+        slug: "docs/flutter/guides/status-indicators",
+      },
+      { label: "Dynamic Theming", slug: "docs/flutter/guides/dynamic-theming" },
+      { label: "More Guides", slug: "docs/flutter/guides/more-guides" },
     ],
   },
 ] satisfies StarlightUserConfig["sidebar"];
